e8ab78fd8b9f7d061ba6555b0238846f2693c863
John Baldwin <jlbaldwin@gmail.com>
2014-02-23 16:07:05 -0500

    still doing basic configuring. db:create and db:migrate running now with local postgres database

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,7 @@
 Myapp::Application.routes.draw do
+  get "welcome/index"
+
+  get '/welcome' => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
