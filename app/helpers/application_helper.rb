67fb8a985a2131a3e10e38b87047fed4a4e58bdf
John Baldwin <jlbaldwin@gmail.com>
2014-03-04 22:24:22 -0500

    ran db:migrate, updated schema. added gems for working with file uploads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  #
+  #  useage
+  # link_to 'Title', xyz_path, class: active_page('action_name')
+  #
+  def active_page(page_name)
+    'active' if params[:action] == page_name
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225175448) do
+ActiveRecord::Schema.define(version: 20140303023334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.boolean  "all_day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "name"
